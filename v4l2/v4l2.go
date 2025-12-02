61c8c404f56ad415b7cafa458889e34bf2f273bf
alaflatca <alaflatca@gmail.com>
2025-12-02 07:14:54 +0000

    not working READ

--- v4l2/v4l2.go
+++ v4l2/v4l2.go
@@ -3,6 +3,7 @@ package v4l2
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"unsafe"
 
 	"golang.org/x/sys/unix"
@@ -28,7 +29,33 @@ const (
 	CapVideoCapture = 0x00000001
 	CapStreaming    = 0x04000000
 	CapDeviceCaps   = 0x80000000
+	CAPReadWrite    = 0x00000002
 )
+
+type PixFormat struct {
+	Width        uint32
+	Height       uint32
+	PixelFormat  uint32
+	Field        uint32
+	BytesPerLine uint32
+	SizeImage    uint32
+	Colorspace   uint32
+	Priv         uint32
+	Flags        uint32
+	YcbcrEnc     uint32
+	Quantization uint32
+	XferFunc     uint32
+}
+
+type Format struct {
+	Type uint32
+	_    uint32
+	fmt  [200]byte
+}
+
+func (f *Format) Pix() *PixFormat {
+	return (*PixFormat)(unsafe.Pointer(&f.fmt[0]))
+}
 
 // 비트들 한 숫자로 합치기 (겹침 x)
 func ioc(dir, typ, nr, size uintptr) uintptr {
@@ -41,6 +68,32 @@ func ioc(dir, typ, nr, size uintptr) uintptr {
 func ior(typ, nr, size uintptr) uintptr {
 	return ioc(iocRead, typ, nr, size)
 }
+func iow(typ, nr, size uintptr) uintptr {
+	return ioc(iocWrite, typ, nr, size)
+}
+func iowr(typ, nr, size uintptr) uintptr {
+	return ioc(iocRead|iocWrite, typ, nr, size)
+}
+
+const (
+	BufTypeVideoCapture = 1
+)
+const (
+	FieldAny  = 0
+	FieldNone = 1
+)
+
+func fourCC(a, b, c, d byte) uint32 {
+	return uint32(a) |
+		uint32(b)<<8 |
+		uint32(c)<<16 |
+		uint32(d)<<24
+}
+
+var (
+	PixFmtYUYV  = fourCC('Y', 'U', 'Y', 'V')
+	PixFmtMJPEG = fourCC('M', 'J', 'P', 'G')
+)
 
 func ioctl(fd uintptr, req uintptr, arg unsafe.Pointer) error {
 	_, _, errno := unix.Syscall(unix.SYS_IOCTL, fd, req, uintptr(arg))
@@ -51,7 +104,7 @@ func ioctl(fd uintptr, req uintptr, arg unsafe.Pointer) error {
 }
 
 func cString(b []byte) string {
-	if i := bytes.IndexByte(b, 0); i > 0 {
+	if i := bytes.IndexByte(b, 0); i >= 0 {
 		b = b[:i]
 	}
 	return string(b)
@@ -75,7 +128,6 @@ func Start(fd uintptr) error {
 		0,
 		uintptr(unsafe.Sizeof(caps)),
 	)
-	fmt.Printf("VIDIOC_QUERYCAP req = 0x%x\n", reqQueryCap)
 
 	if err := ioctl(fd, reqQueryCap, unsafe.Pointer(&caps)); err != nil {
 		return err
@@ -89,12 +141,21 @@ func Start(fd uintptr) error {
 	fmt.Printf("%-16s : %#x\n", "device_caps", caps.DeviceCaps)
 
 	devCaps := caps.Capabilities
-	if caps.Capabilities&caps.DeviceCaps != 0 {
+	// 이 디바이스는 device_caps를 쓰는 신형 스타일이다 라는 신호호
+	if caps.Capabilities&CapDeviceCaps != 0 {
 		devCaps = caps.DeviceCaps
+	}
+
+	if devCaps&CAPReadWrite == 0 {
+		return fmt.Errorf("device does not support READ WRITE")
+	} else {
+		fmt.Println("=> device supports READ WRITE")
 	}
 
 	if devCaps&CapVideoCapture == 0 {
 		return fmt.Errorf("device does not support VIDEO_CAPTURE")
+	} else {
+		fmt.Println("=> device supports VIDEO CAPTURE")
 	}
 
 	if devCaps&CapStreaming == 0 {
@@ -111,5 +172,56 @@ func Start(fd uintptr) error {
 }
 
 func setFormatAndReadOneFrame(fd uintptr) error {
+	var fmtV4L2 Format
+	fmtV4L2.Type = BufTypeVideoCapture
+
+	pix := fmtV4L2.Pix()
+
+	pix.Width = 640
+	pix.Height = 480
+	// pix.PixelFormat = PixFmtMJPEG
+	pix.PixelFormat = PixFmtYUYV
+	pix.Field = FieldNone
+
+	reqSetFmt := iowr(
+		uintptr('V'),
+		5,
+		uintptr(unsafe.Sizeof(fmtV4L2)),
+	)
+
+	fmt.Printf("VIDIOC_S_FMT req = %#x\n", reqSetFmt)
+
+	if err := ioctl(fd, reqSetFmt, unsafe.Pointer(&fmtV4L2)); err != nil {
+		return fmt.Errorf("VIDIOC_S_FMT failed: %v", err)
+	}
+
+	fmt.Println("==S_FMT result==")
+	fmt.Printf("size		: %d x %d\n", pix.Width, pix.Height)
+	fmt.Printf("pixelformat	: %#x\n", pix.PixelFormat)
+	fmt.Printf("bytesperline: %d\n", pix.BytesPerLine)
+	fmt.Printf("sizeimage	: %d\n", pix.SizeImage)
+
+	if pix.SizeImage == 0 {
+		return fmt.Errorf("SizeImage == 0, cannot read freme size")
+	}
+
+	buf := make([]byte, pix.SizeImage)
+
+	n, err := unix.Read(int(fd), buf)
+	if err != nil {
+		return fmt.Errorf("read frame failed: %v", err)
+	}
+
+	fmt.Printf("read() got %d bytes (expected ~%d)\n", n, pix.SizeImage)
+
+	f, err := os.Create("one_frame")
+	if err != nil {
+		return fmt.Errorf("one_frame open failed: %v", err)
+	}
+	defer f.Close()
+	if _, err = f.Write(buf[:n]); err != nil {
+		return fmt.Errorf("one_frame write failed: %v", err)
+	}
+
 	return nil
 }
